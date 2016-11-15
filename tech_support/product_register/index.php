90762f6677f3a28f3a13c9cdb58075b4216ce2c1
jcooprider <jcooprider@bentley.edu>
2016-11-14 19:58:32 -0500

    Add files via upload

---
+++ tech_support/product_register/index.php
@@ -0,0 +1,32 @@
+<?php
+require('../model/database.php');
+require('../model/customer_db.php');
+require('../model/product_db.php');
+require('../model/registration_db.php');
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action === NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action === NULL) {
+        $action = 'login_customer';
+    }
+}
+
+//instantiate variable(s)
+$email = '';
+
+if ($action == 'login_customer') {
+    include('customer_login.php');
+} else if ($action == 'get_customer') {
+    $email = filter_input(INPUT_POST, 'email');
+    $customer = get_customer_by_email($email);
+    $products = get_products();
+    include('product_register.php');
+} else if ($action == 'register_product') {
+    $customer_id = filter_input(INPUT_POST, 'customer_id', FILTER_VALIDATE_INT);
+    $product_code = filter_input(INPUT_POST, 'product_code');
+    add_registration($customer_id, $product_code);
+    $message = "Product ($product_code) was registered successfully.";
+    include('product_register.php');
+}
+?>
