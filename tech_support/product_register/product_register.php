06a491c0d56481c5a4b94fe3e6eeaf1814ad16ee
steeledela <steele_dela@bentley.edu>
2016-12-05 15:20:45 -0500

    Update product_register.php

--- tech_support/product_register/product_register.php
+++ tech_support/product_register/product_register.php
@@ -1,11 +1,14 @@
-<?php include '../view/header.php'; ?>
+<?php include '../view/header.php'; 
+    include '../model/database.php';
+    include '../model/product_db.php';
+    $result=get_products();?>
 <main>
 
     <h2>Register Product</h2>
     <?php if (isset($message)) : ?>
         <p><?php echo $message; ?></p>
     <?php else: ?>
-        <form action="." method="post" id="aligned">
+        <form action="product_register_execute.php" method="post" id="aligned">
             <input type="hidden" name="action" 
                    value="register_product">
             <input type="hidden" name="customer_id" 
@@ -18,7 +21,7 @@
 
             <label>Product:</label>
             <select name="product_code">
-            <?php foreach ($products as $product) : ?>
+            <?php foreach ($result as $product) : ?>
                 <option value="<?php echo htmlspecialchars($product['productCode']); ?>">
                     <?php echo htmlspecialchars($product['name']); ?>
                 </option>
