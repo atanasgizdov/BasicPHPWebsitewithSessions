15b00f91dcb9d7d92da78fe4552d48880251dbf8
steeledela <steele_dela@bentley.edu>
2016-12-15 17:24:44 -0500

    Update index.php

--- tech_support/product_register/index.php
+++ tech_support/product_register/index.php
@@ -5,38 +5,47 @@ require('../model/product_db.php');
 require('../model/registration_db.php');
 include '../model/session_checker.php';
 
-
-// disalow entry if not an admin
 if (isset($_SESSION['loggedin']) AND $_SESSION['loggedin'] == true) {
-
-if (isset($_SESSION['usertype']) AND ($_SESSION['usertype'] == "admin")) {
-	
-}
+    if (isset($_SESSION['usertype']) AND ($_SESSION['usertype'] == "customer")) {
+        $action='get_customer';	
+    }   
 else {
 	header("location: view/invalidpermissions.php?".$_SESSION['usertype']);
-	
 	}
-
 }
-
-$action = filter_input(INPUT_POST, 'action');
-if ($action === NULL) {
-    $action = filter_input(INPUT_GET, 'action');
+else {
+    $action = filter_input(INPUT_POST, 'action');
     if ($action === NULL) {
-        $action = 'login_customer';
-    }
+        $action = filter_input(INPUT_GET, 'action');
+        if ($action === NULL) {
+            $action = 'login_customer';
+        }
+    }  
 }
+
 
 //instantiate variable(s)
 $email = '';
 
 if ($action == 'login_customer') {
     include('customer_login.php');
+
 } else if ($action == 'get_customer') {
-    $email = filter_input(INPUT_POST, 'email');
-    $customer = get_customer_by_email($email);
-    $products = get_products();
-    include('product_register.php');
+    if (isset($_SESSION['email'])){
+        $email=$_SESSION['email'];     
+        $customer=get_customer_by_email($email);
+    } else{
+        $email = filter_input(INPUT_POST, 'email');
+        $password = filter_input(INPUT_POST,'password');
+        $customer = get_customer_by_email($email);
+        if ($customer['password']==$password){
+            $_SESSION['loggedin']=true;
+            $_SESSION['usertype']='customer';
+            $_session['email']=$email;
+            include('product_register.php');}
+        else {echo "Invalid Login";
+            include ('customer_loging.php');}}
+
 } else if ($action == 'register_product') {
     $customer_id = filter_input(INPUT_POST, 'customer_id', FILTER_VALIDATE_INT);
     $product_code = filter_input(INPUT_POST, 'product_code');
